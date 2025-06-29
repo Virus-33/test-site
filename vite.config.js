1365e581fa68e4e13def1e1218177f6c5fc14ec6
Virus-33 <hazhdachka.tv@gmail.com>
2025-06-29 16:23:23 +0700

    small style changes, new td quotes and TOC

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,23 @@ import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 import remarkGfm from 'remark-gfm'
 import rehypeSlug from "rehype-slug";
+import {remarkTableOfContents} from "remark-table-of-contents";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), mdx(
       {
-        remarkPlugins: [remarkGfm],
+        remarkPlugins: [remarkGfm, [remarkTableOfContents, {
+            containerAttributes: {
+                id: 'articleToc'
+            },
+            containerTagName: '',
+            minDepth: 2
+        }
+        ]],
         rehypePlugins: [rehypeSlug]
       }
   )],
   pages: mdx()
 })
+
